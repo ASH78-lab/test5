94388e5f1bb47bb24af6cc570945055e2dc5fa3f
ASH78-lab <roben4300@gmail.com>
2026-03-02 10:16:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,33 @@ from selenium.webdriver.support import expected_conditions as EC
 import time
 from selenium.webdriver.common.keys import Keys
 
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
-
-chrome_options = webdriver.ChromeOptions()
+#chrome_options = webdriver.ChromeOptions()
+chrome_options = Options()
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-gpu")
+#driver = webdriver.Chrome(options=chrome_options)
+
+
+
+###########################################################################################
+
+
+# Define proxy (ip:port)
+PROXY = "195.158.8.123:3128" 
+
+chrome_options.add_argument(f'--proxy-server={PROXY}')
+
+# Initialize driver with options
 driver = webdriver.Chrome(options=chrome_options)
 driver.set_window_size(1800, 1000) 
+driver.get("https://www.whoscored.com/")
 #####################################################################################################
-y='https://www.meteopt.com/modelos/gfs?lat=55.752&lon=37.616&lang=en'
 
-driver.get(y)
 time.sleep(15)
 
 a=driver.find_element(By.TAG_NAME,'body')
